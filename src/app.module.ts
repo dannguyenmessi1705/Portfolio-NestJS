96a60b4a9b6e0f4116aba316ed360cae22ea56bb
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-30 02:32:17 +0700

    feat: config connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,25 @@ import { SocketModule } from './socket/socket.module';
 import { BlogsModule } from './blogs/blogs.module';
 import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm.config';
 
 @Module({
-  imports: [SendgridModule, SocketModule, BlogsModule, ProjectsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    SendgridModule,
+    SocketModule,
+    BlogsModule,
+    ProjectsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,67 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+// STATIC FILES
+import { fastifyStatic } from '@fastify/static';
+import { join } from 'path';
+import { rootDir } from './utils/rootDir';
+
+import helmet from '@fastify/helmet';
+
+import compress from '@fastify/compress';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: {
+        file: 'public/logs/app.log',
+      },
+    }),
+  );
+
+  app.register(fastifyStatic, {
+    root: join(rootDir, 'public'),
+    prefix: '/public/', // Prefix để truy cập file tĩnh
+  });
+
+  app.register(helmet);
+
+  app.register(compress, { encodings: ['gzip', 'deflate'] });
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept-Language',
+      'X-Requested-With',
+      'Accept',
+      'Origin',
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Methods',
+      'Access-Control-Allow-Credentials',
+      'Access-Control-Max-Age',
+      'Access-Control-Expose-Headers',
+      'Access-Control-Request-Headers',
+    ],
+  }); // Enable CORS
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  await app.listen(3001);
 }
 bootstrap();
