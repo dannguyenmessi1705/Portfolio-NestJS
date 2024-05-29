18dcd135edc58626f45acce6b94517c3dfb76f38
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-30 03:47:43 +0700

    feat: config Socketio

--- src/socket/socket.gateway.service.ts
+++ src/socket/socket.gateway.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 
-@Injectable()
-export class SocketGatewayService {}
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: true,
+  },
+  maxHttpBufferSize: 1e8,
+})
+export class SocketGatewayService
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+  afterInit(server: Socket) {}
+
+  async handleConnection(socket: Socket) {
+    try {
+      console.log(`Client connected: ${socket.id}`);
+    } catch (error) {
+      socket.disconnect();
+    }
+  }
+
+  async handleDisconnect(socket: Socket) {
+    console.log(`Client disconnected: ${socket.id}`);
+  }
+
+  @SubscribeMessage('message')
+  async handleListenMessage(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() data: any,
+  ) {
+    console.log(`Message Received from ${socket.id}`, data);
+  }
+
+  handleEmitMEssage(data: any, event: string) {
+    this.server.emit(event, data);
+  }
+}
