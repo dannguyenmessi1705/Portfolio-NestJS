8888b2cb436ab92a0c6b861a782edff193807b15
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-31 10:34:51 +0700

    refractor: use guard to protected route

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,12 +1,21 @@
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProjectsService } from './services/projects.service';
 import { NewProjectDto } from './dtos/create-project.dto';
 import { File, FileInterceptor } from '@nest-lab/fastify-multer';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private projectService: ProjectsService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   @UseInterceptors(FileInterceptor('image'))
   async createProject(@Body() body: NewProjectDto, @UploadedFile() file: File) {
